92c27249fa8c53f0e8f1e029e89420b0c24f87f4
Archa K <1066@trenser.com>
2018-11-12 08:53:25 +0530

    Added css

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,44 @@
 const path = require('path');
-module.exports = {
-    entry : './src/app.js',
-    output : {
-        path : path.join(__dirname,'pub'),
-        filename : 'bundle.js'
-    },
-    module : 
-    {
-        rules: [
-            {
-                loader : 'babel-loader',
-                test : /\.js$/,
-                exclude : /node_modules/
-            },
-            {
-                test : /\.s?css$/,
-                use : [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool : 'cheap-module-eval-source-map',
-    devServer : {
-        contentBase :path.join(__dirname,'pub'),
-        historyApiFallback : true
-    }
-};
+const ExtractTextPlugin =  require('extract-text-webpack-plugin');
+
+module.exports = (env) =>
+{
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin();
+
+    console.log("env",env);
+    return {
+        entry : './src/app.js',
+        output : {
+            path : path.join(__dirname,'pub'),
+            filename : 'bundle.js'
+        },
+        module : 
+        {
+            rules: [
+                {
+                    loader : 'babel-loader',
+                    test : /\.js$/,
+                    exclude : /node_modules/
+                },
+                {
+                    test : /\.s?css$/,
+                    use : CSSExtract.extract({
+                        use: [
+                            'css-loader',
+                            'sass-loader'
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins : [
+            CSSExtract
+        ],
+        devtool : isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer : {
+            contentBase :path.join(__dirname,'pub'),
+            historyApiFallback : true
+        }
+    };
+}
